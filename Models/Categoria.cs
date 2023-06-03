a71f177b3dd4ccd5c364c4fd7909573281ac6b77
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2023-06-03 15:27:03 -0500

    Clase 9 - mapeo de los datos

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace proyectoEF.Models;
 
+[Table("Categoria")]
 public class Categoria
 {
+  [Key]
   public Guid CategoriaId { get; set; }
 
+  [Required]
+  [MaxLength(150)]
   public string Nombre { get; set; }
 
   public string Descripcion { get; set; }
